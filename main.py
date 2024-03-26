7fbdec59a102215dd4cd62062853842db8d951d2
Robert <rvhuben@gmail.com>
2024-03-26 15:11:07 -0400

    minor refactoring/cleaning

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import torch
 from torchvision import models, transforms
 from PIL import Image
 
+model = models.resnet18(pretrained=True)
+model.eval()  # Set the model to evaluation mode
+
+# def optimize_towards_class(input_image, model, num_iter=)
 
 def load_file_path_to_tensor(path_to_image):
     # Define the transformation to apply to the image
@@ -16,29 +20,22 @@ def load_file_path_to_tensor(path_to_image):
     image = image.unsqueeze(0)  # Add a batch dimension
     return image
 
-def classify(input_tensor):
+def classify(image_path):
     # simple method written by ChatGPT to start the classifier off
     # Load a pre-trained model, for example, ResNet18
-    model = models.resnet18(pretrained=True)
-    model.eval()  # Set the model to evaluation mode
 
     image=load_file_path_to_tensor(image_path)
-
     # Perform the classification
     with torch.no_grad():  # Disable gradient calculation for inference
         outputs = model(image)
         _, predicted = outputs.max(1)
 
-    # Decode the prediction
-    # Load the labels (ensure you have the ImageNet labels file if using an ImageNet model)
-    imagenet_labels = load_labels()
-
     # Get the predicted class label
+    imagenet_labels = load_labels()
     predicted_class = imagenet_labels[predicted.item()]
-
     print(f"Predicted class: {predicted_class}")
 
-def loss_from_given_class(image_as_tensor, model, target_class):
+def loss_from_given_class(image_as_tensor, target_class):
     model_output = model(image_as_tensor)
     loss=-1*torch.log(model_output[target_class])
     return loss
@@ -51,6 +48,7 @@ def load_labels():
         imagenet_labels = [line.split(", ")[1].strip() for line in f.readlines()]
     return imagenet_labels
 
-for image_name in ["bird", "cat", "shark", "snake"]:
-    image_path=f"inputs/{image_name}.jpg"
-    classify(image_path)    
+if __name__=="__main__":
+    for image_name in ["bird", "cat", "shark", "snake"]:
+        image_path=f"inputs/{image_name}.jpg"
+        classify(image_path)    
