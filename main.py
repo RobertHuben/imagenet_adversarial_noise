27dd5a5af2ae7e620062793f43e4a2eefc20dfa9
Robert <rvhuben@gmail.com>
2024-03-26 15:22:48 -0400

    added loop to optimize image adversarially.

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ from PIL import Image
 model = models.resnet18(pretrained=True)
 model.eval()  # Set the model to evaluation mode
 
-# def optimize_towards_class(input_image, model, num_iter=)
+def optimize_towards_class(original_image, target_class, num_iter=10, alpha=1e-3):
+    noised_image=torch.clone(original_image)
+    noised_image.requires_grad_() #we'll be optimizing this to add noise
+
+    optimizer=torch.optim.Adam(params=[noised_image], lr=0.0001)
+    for _ in range(num_iter):
+        optimizer.zero_grad()
+        loss=loss_from_given_class(noised_image, target_class)+alpha*loss_l2(noised_image, original_image)
+        loss.backward()
+        optimizer.step()
+    
+
 
 def load_file_path_to_tensor(path_to_image):
     # Define the transformation to apply to the image
@@ -20,11 +31,8 @@ def load_file_path_to_tensor(path_to_image):
     image = image.unsqueeze(0)  # Add a batch dimension
     return image
 
-def classify(image_path):
-    # simple method written by ChatGPT to start the classifier off
-    # Load a pre-trained model, for example, ResNet18
 
-    image=load_file_path_to_tensor(image_path)
+def classify(image_as_tensor):
     # Perform the classification
     with torch.no_grad():  # Disable gradient calculation for inference
         outputs = model(image)
@@ -37,10 +45,10 @@ def classify(image_path):
 
 def loss_from_given_class(image_as_tensor, target_class):
     model_output = model(image_as_tensor)
-    loss=-1*torch.log(model_output[target_class])
+    loss=-1*torch.log(model_output[0,target_class])
     return loss
 
-def l2_loss(image_as_tensor, original_image_as_tensor):
+def loss_l2(image_as_tensor, original_image_as_tensor):
     return torch.norm(image_as_tensor-original_image_as_tensor, p=2)
 
 def load_labels():
@@ -51,4 +59,6 @@ def load_labels():
 if __name__=="__main__":
     for image_name in ["bird", "cat", "shark", "snake"]:
         image_path=f"inputs/{image_name}.jpg"
-        classify(image_path)    
+        image=load_file_path_to_tensor(image_path)
+        optimize_towards_class(image, 10)
+        classify(image)    
