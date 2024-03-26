6edc240c4a4b1389736aa10fb198157704524844
Robert <rvhuben@gmail.com>
2024-03-26 15:38:38 -0400

    finished the optimize_towards_class method, including printing the state as it goes and doing a final evaluation

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import torch
-from torchvision import models, transforms
+from torchvision import models, transforms, utils
 from PIL import Image
 
 model = models.resnet18(pretrained=True)
 model.eval()  # Set the model to evaluation mode
 
+def model_probabilities(input):
+    return torch.softmax(model(input), dim=1).flatten()
+
 def optimize_towards_class(original_image, target_class, num_iter=10, alpha=1e-3):
     noised_image=torch.clone(original_image)
     noised_image.requires_grad_() #we'll be optimizing this to add noise
 
-    optimizer=torch.optim.Adam(params=[noised_image], lr=0.0001)
-    for _ in range(num_iter):
+    optimizer=torch.optim.Adam(params=[noised_image], lr=0.01)
+    print("Before noise is added, my original prediction is:")
+    classify(original_image)
+    for step_num in range(num_iter):
         optimizer.zero_grad()
         loss=loss_from_given_class(noised_image, target_class)+alpha*loss_l2(noised_image, original_image)
         loss.backward()
         optimizer.step()
-    
+        class_probability=model_probabilities(noised_image)[target_class]
+        print(f"After {step_num} steps, the chance of this class being {target_class} is {class_probability}")
+    print("After all the steps I predict the object to be:")
+    classify(noised_image)
+    utils.save_image(noised_image[0],"foo.jpg")
 
 
 def load_file_path_to_tensor(path_to_image):
@@ -24,7 +33,6 @@ def load_file_path_to_tensor(path_to_image):
         transforms.Resize(256),  # Resize the image to 256x256 pixels
         transforms.CenterCrop(224),  # Crop the image to 224x224 pixels from the center
         transforms.ToTensor(),  # Convert the image to a PyTorch tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize the image
     ])
     image = Image.open(path_to_image)
     image = transform(image)
@@ -35,24 +43,24 @@ def load_file_path_to_tensor(path_to_image):
 def classify(image_as_tensor):
     # Perform the classification
     with torch.no_grad():  # Disable gradient calculation for inference
-        outputs = model(image)
-        _, predicted = outputs.max(1)
+        outputs = model_probabilities(image_as_tensor)
+        prob, class_num = outputs.max(0)
 
     # Get the predicted class label
     imagenet_labels = load_labels()
-    predicted_class = imagenet_labels[predicted.item()]
-    print(f"Predicted class: {predicted_class}")
+    predicted_class = imagenet_labels[class_num.item()]
+    print(f"Predicted class: {predicted_class}, probability: {prob.item():.2%}")
 
 def loss_from_given_class(image_as_tensor, target_class):
-    model_output = model(image_as_tensor)
-    loss=-1*torch.log(model_output[0,target_class])
+    model_output = model_probabilities(image_as_tensor)
+    loss=-1*torch.log(model_output[target_class])
     return loss
 
 def loss_l2(image_as_tensor, original_image_as_tensor):
     return torch.norm(image_as_tensor-original_image_as_tensor, p=2)
 
 def load_labels():
-    with open("imagenet_classes.txt") as f:  # You need to have this file with the class names
+    with open("imagenet_classes.txt") as f:
         imagenet_labels = [line.split(", ")[1].strip() for line in f.readlines()]
     return imagenet_labels
 
